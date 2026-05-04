c7db43713194b4fba6974b3f6cc3b58f4092f04d
Sunyuntao Xu <sx120@ic.ac.uk>
2026-05-04 17:03:44 +0100

    Create PB1_PB2_file_combine.sh

---
+++ PB1_PB2_file_combine.sh
@@ -0,0 +1,5 @@
+cd E:\influenza
+
+Get-Content China_PB1_NT.fasta, UK_PB1_NT.fasta | Set-Content China_UK_PB1_NT.fasta
+
+Get-Content China_PB2_NT.fasta, UK_PB2_NT.fasta | Set-Content China_UK_PB2_NT.fasta
