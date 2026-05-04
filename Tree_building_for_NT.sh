a8df9a9325d97de64633c8e334789707cf142a52
Sunyuntao Xu <sx120@ic.ac.uk>
2026-05-04 19:00:31 +0100

    Add script for building PB1 NT tree using IQ-TREE

---
+++ Tree_building_for_NT.sh
@@ -0,0 +1,20 @@
+# PB1 NT tree
+& "C:\iqtree-2.4.0-Windows\bin\iqtree2.exe" `
+  -s "E:\influenza\China_UK_PB1_NT_aligned_trimmed.fasta" `
+  -st DNA `
+  -m MFP `
+  -B 1000 `
+  -alrt 1000 `
+  -nt AUTO `
+  -pre "E:\influenza\IQTREE_NT\IQTREE_PB1_NT_trimmed"
+
+
+  # PB1 NT tree
+& "C:\iqtree-2.4.0-Windows\bin\iqtree2.exe" `
+  -s "E:\influenza\China_UK_PB1_NT_aligned_trimmed.fasta" `
+  -st DNA `
+  -m MFP `
+  -B 1000 `
+  -alrt 1000 `
+  -nt AUTO `
+  -pre "E:\influenza\IQTREE_NT\IQTREE_PB1_NT_trimmed"
