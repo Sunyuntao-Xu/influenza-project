3060bd6ffeb3fca0c3c2650b64e3653202669937
Sunyuntao Xu <sx120@ic.ac.uk>
2026-05-04 19:04:03 +0100

    Update Tree_building_for_NT.sh

--- Tree_building_for_NT.sh
+++ Tree_building_for_NT.sh
@@ -1,3 +1,15 @@
+# Remove duplicate names
+seqkit rmdup -n `
+  -D "E:\influenza\PB1_NT_duplicate_names.txt" `
+  "E:\influenza\China_UK_PB1_NT_aligned_trimmed.fasta" `
+  -o "E:\influenza\China_UK_PB1_NT_aligned_trimmed_dedup.fasta"
+
+seqkit rmdup -n `
+  -D "E:\influenza\PB2_NT_duplicate_names.txt" `
+  "E:\influenza\China_UK_PB2_NT_aligned_trimmed.fasta" `
+  -o "E:\influenza\China_UK_PB2_NT_aligned_trimmed_dedup.fasta"
+
+
 # PB1 NT tree
 & "C:\iqtree-2.4.0-Windows\bin\iqtree2.exe" `
   -s "E:\influenza\China_UK_PB1_NT_aligned_trimmed.fasta" `
