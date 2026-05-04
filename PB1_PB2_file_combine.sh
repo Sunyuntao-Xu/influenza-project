304b43a97cc118108e2aa0cc82e4948dca1cc727
Sunyuntao Xu <sx120@ic.ac.uk>
2026-05-04 18:48:51 +0100

    Update PB1_PB2_file_combine.sh

--- PB1_PB2_file_combine.sh
+++ PB1_PB2_file_combine.sh
@@ -5,10 +5,10 @@ Get-Content China_PB1_NT.fasta, UK_PB1_NT.fasta | Set-Content China_UK_PB1_NT.fa
 Get-Content China_PB2_NT.fasta, UK_PB2_NT.fasta | Set-Content China_UK_PB2_NT.fasta
 
 # Remove problematic sequences based on length
-seqkit seq -m 2000 -M 2341 China_UK_PB1_NT.fasta -o China_UK_PB1_NT_lenfiltered.fasta
+seqkit seq -m 2000 -M 2400 China_UK_PB1_NT.fasta -o China_UK_PB1_NT_lenfiltered.fasta
 seqkit stats China_UK_PB1_NT.fasta China_UK_PB1_NT_lenfiltered.fasta
 
-seqkit seq -m 2000 -M 2341 China_UK_PB2_NT.fasta -o China_UK_PB2_NT_lenfiltered.fasta
+seqkit seq -m 2000 -M 2400 China_UK_PB2_NT.fasta -o China_UK_PB2_NT_lenfiltered.fasta
 seqkit stats China_UK_PB2_NT.fasta China_UK_PB2_NT_lenfiltered.fasta
 
 
