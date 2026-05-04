d20173444b2615877fe0ae1f92003a9e211befbb
Sunyuntao Xu <sx120@ic.ac.uk>
2026-05-04 19:06:31 +0100

    Update script to use deduplicated fasta files

--- Tree_building_for_NT.sh
+++ Tree_building_for_NT.sh
@@ -1,32 +1,37 @@
 # Remove duplicate names
+
 seqkit rmdup -n `
   -D "E:\influenza\PB1_NT_duplicate_names.txt" `
   "E:\influenza\China_UK_PB1_NT_aligned_trimmed.fasta" `
   -o "E:\influenza\China_UK_PB1_NT_aligned_trimmed_dedup.fasta"
+
+# [INFO] 27 duplicated records removed
 
 seqkit rmdup -n `
   -D "E:\influenza\PB2_NT_duplicate_names.txt" `
   "E:\influenza\China_UK_PB2_NT_aligned_trimmed.fasta" `
   -o "E:\influenza\China_UK_PB2_NT_aligned_trimmed_dedup.fasta"
 
+# [INFO] 26 duplicated records removed
+
 
 # PB1 NT tree
 & "C:\iqtree-2.4.0-Windows\bin\iqtree2.exe" `
-  -s "E:\influenza\China_UK_PB1_NT_aligned_trimmed.fasta" `
+  -s "E:\influenza\China_UK_PB1_NT_aligned_trimmed_dedup.fasta" `
   -st DNA `
   -m MFP `
   -B 1000 `
   -alrt 1000 `
   -nt AUTO `
-  -pre "E:\influenza\IQTREE_NT\IQTREE_PB1_NT_trimmed"
+  -pre "E:\influenza\IQTREE_NT\IQTREE_PB1_NT_trimmed_dedup"
 
 
   # PB1 NT tree
 & "C:\iqtree-2.4.0-Windows\bin\iqtree2.exe" `
-  -s "E:\influenza\China_UK_PB1_NT_aligned_trimmed.fasta" `
+  -s "E:\influenza\China_UK_PB1_NT_aligned_trimmed_dedup.fasta" `
   -st DNA `
   -m MFP `
   -B 1000 `
   -alrt 1000 `
   -nt AUTO `
-  -pre "E:\influenza\IQTREE_NT\IQTREE_PB1_NT_trimmed"
+  -pre "E:\influenza\IQTREE_NT\IQTREE_PB1_NT_trimmed_dedup"
