7ec12d2390d5e998f434c82f881cc762d94912dc
Sunyuntao Xu <sx120@ic.ac.uk>
2026-05-05 18:34:08 +0100

    Update PB1 to PB2 in tree building script

--- Tree_building_for_NT.sh
+++ Tree_building_for_NT.sh
@@ -26,12 +26,12 @@ seqkit rmdup -n `
   -pre "E:\influenza\IQTREE_NT\IQTREE_PB1_NT_trimmed_dedup"
 
 
-  # PB1 NT tree
+# PB2 NT tree
 & "C:\iqtree-2.4.0-Windows\bin\iqtree2.exe" `
-  -s "E:\influenza\China_UK_PB1_NT_aligned_trimmed_dedup.fasta" `
+  -s "E:\influenza\China_UK_PB2_NT_aligned_trimmed_dedup.fasta" `
   -st DNA `
   -m MFP `
   -B 1000 `
   -alrt 1000 `
   -nt AUTO `
-  -pre "E:\influenza\IQTREE_NT\IQTREE_PB1_NT_trimmed_dedup"
+  -pre "E:\influenza\IQTREE_NT\IQTREE_PB2_NT_trimmed_dedup"
