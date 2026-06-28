cefb98f6de5917c580a327516df03c522174c5e7
Sunyuntao Xu <sx120@ic.ac.uk>
2026-06-28 19:03:08 +0100

    Trim aligned columns for PA and update stats

--- trim.sh
+++ trim.sh
@@ -7,8 +7,10 @@ seqkit subseq -r 51:2400 China_UK_PB2_NT_aligned.fasta -o China_UK_PB2_NT_aligne
 seqkit subseq -r 45:2400 China_UK_PB1_NT_aligned.fasta -o China_UK_PB1_NT_aligned_trimmed.fasta
 
 # PA: trim aligned columns 45 to 2400
-seqkit subseq -r 45:2400 China_UK_PB1_NT_aligned.fasta -o China_UK_PB1_NT_aligned_trimmed.fasta
+seqkit subseq -r 113:2263 China_UK_PA_NT_aligned.fasta -o China_UK_PA_NT_aligned_trimmed.fasta
 
 seqkit stats China_UK_PB2_NT_aligned.fasta China_UK_PB2_NT_aligned_trimmed.fasta
 
 seqkit stats China_UK_PB1_NT_aligned.fasta China_UK_PB1_NT_aligned_trimmed.fasta
+
+seqkit stats China_UK_PA_NT_aligned.fasta China_UK_PA_NT_aligned_trimmed.fasta
